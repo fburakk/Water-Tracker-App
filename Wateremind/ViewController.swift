79543ebc2bea54b4ed5cbbb91530813584ae1fa1
Burak Köse <iosdeveloper19@hotmail.com>
2022-07-31 18:16:13 +0300

    Initial Commit

---
+++ Wateremind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wateremind
+//
+//  Created by Burak Köse on 31.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
